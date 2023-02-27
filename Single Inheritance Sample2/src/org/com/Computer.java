4458a6e1e494653ee35febf9f169a54459dd67ec
Nithyaranjini <srinithya2k@gmail.com>
2023-02-27 16:37:48 +0530

    commit

---
+++ Single Inheritance Sample2/src/org/com/Computer.java
@@ -0,0 +1,9 @@
+package org.com;
+
+public class Computer {
+	public void computerModel() {
+		System.out.println("My computer model is lenova");
+
+	}
+
+}
---
+++ Single Inheritance Sample2/src/org/com/Desktop.java
@@ -0,0 +1,16 @@
+package org.com;
+
+public class Desktop extends Computer{
+	private void desktopSize() {
+	 System.out.println("Desktop size is 15.6inches(39.62");}
+	public static void main(String[] args) {
+		Desktop c = new Desktop();
+		c.desktopSize();
+		c.computerModel();
+		
+	}
+	 
+
+	}
+
+
